d6cb497d5aa37631c3e36d85f71c99946a6eed94
Keeven Oliveira <keevinhuholiveira@gmail.com>
2022-07-11 01:18:55 -0300

    [UPDATE]: Implementing anothers jests

--- src/lib/calculator.js
+++ src/lib/calculator.js
@@ -1,1 +1,13 @@
-module.exports.sum = (a, b) => a + b;
+module.exports.sum = (a, b) => {
+
+    //you can use + before the number to convert it to a number
+
+    const firstNumber = parseFloat(a);
+    const secondNumber = parseFloat(b);
+
+    if(isNaN(firstNumber) || isNaN(secondNumber)) {
+        throw new Error('Invalid input');
+    }
+
+    return firstNumber + secondNumber // + converts to number
+};
