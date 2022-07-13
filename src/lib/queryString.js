783b8726b6a290e793d1ceed8c012a02b677ef09
Keeven Oliveira <keevinhuholiveira@gmail.com>
2022-07-13 01:11:10 -0300

    [UPDATE]: Refactoring code

--- src/lib/queryString.js
+++ src/lib/queryString.js
@@ -1,11 +1,13 @@
-module.exports.queryString = obj => {
-  const keys = Object.entries(obj).map(([key, value]) => {
-    if (typeof value === 'object' && !Array.isArray(value)) {
-      throw new Error('Objects are not allowed as values');
-    }
+const keyValueToString = ([key, value]) => {
+  if (typeof value === 'object' && !Array.isArray(value)) {
+    throw new Error('Objects are not allowed as values');
+  }
 
-    return `${key}=${value}`;
-  });
+  return `${key}=${value}`;
+};
+
+module.exports.queryString = obj => {
+  const keys = Object.entries(obj).map(keyValueToString);
 
   return keys.join('&');
 };
