63b86617e78ac35f0008bedea9b30b6637536522
Keeven Oliveira <keevinhuholiveira@gmail.com>
2022-08-14 02:00:01 -0300

    [UPDATE]: Using TDD in ProductCard

--- secondModule/components/ProductCard.unit.spec.js
+++ secondModule/components/ProductCard.unit.spec.js
@@ -3,6 +3,24 @@ import ProductCard from '@/components/ProductCard';
 
 import { makeServer } from '@/miragejs/server';
 
+const mountProductCard = (server) => {
+  const product = server.create('product', {
+    title: 'Relógio bonito',
+    price: '22.00',
+    image:
+      'https://images.unsplash.com/photo-1495857000853-fe46c8aefc30?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=',
+  });
+
+  return {
+    wrapper: mount(ProductCard, {
+      propsData: {
+        product,
+      },
+    }),
+    product,
+  };
+};
+
 describe('ProductCard - unit', () => {
   let server;
   beforeEach(() => {
@@ -13,31 +31,26 @@ describe('ProductCard - unit', () => {
   });
 
   it('should match snapshot', () => {
-    const wrapper = mount(ProductCard, {
-      propsData: {
-        product: server.create('product', {
-          title: 'Relógio bonito',
-          price: '22.00',
-          image:
-            'https://images.unsplash.com/photo-1532667449560-72a95c8d381b?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
-        }),
-      },
-    });
+    const { wrapper } = mountProductCard(server);
+
     expect(wrapper.element).toMatchSnapshot();
   });
 
   it('should mount the component', () => {
-    const wrapper = mount(ProductCard, {
-      propsData: {
-        product: server.create('product', {
-          title: 'Relógio bonito',
-          price: '22.00',
-        }),
-      },
-    });
+    const { wrapper } = mountProductCard(server);
 
     expect(wrapper.vm).toBeDefined();
     expect(wrapper.text()).toContain('Relógio bonito');
     expect(wrapper.text()).toContain('22.00');
+  });
+
+  it('should emit the event addToCart with product object', async () => {
+    const { wrapper, product } = mountProductCard(server);
+
+    await wrapper.find('button').trigger('click');
+
+    expect(wrapper.emitted().addToCart).toBeTruthy();
+    expect(wrapper.emitted().addToCart.length).toBe(1);
+    expect(wrapper.emitted().addToCart[0]).toEqual([{ product }]);
   });
 });
