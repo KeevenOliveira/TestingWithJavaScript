3809fa50664638f0b97bba4631651ed854a941d2
Keeven Oliveira <keevinhuholiveira@gmail.com>
2022-08-17 17:15:47 -0300

    [UPDATE]: Updating ProductList test

--- secondModule/pages/ProductList.integration.spec.js
+++ secondModule/pages/ProductList.integration.spec.js
@@ -1,11 +1,24 @@
 import { mount } from '@vue/test-utils';
+import axios from 'axios';
 
 import ProductList from '.';
 
 import ProductCard from '@/components/ProductCard';
 import Search from '@/components/Search';
+import { makeServer } from '@/miragejs/server';
+
+jest.mock('axios', () => ({
+  get: jest.fn(),
+}));
 
 describe('ProductList - integration', () => {
+  let server;
+  beforeEach(() => {
+    server = makeServer({ environment: 'test' });
+  });
+  afterEach(() => {
+    server.shutdown();
+  });
   it('should mount the component', () => {
     const wrapper = mount(ProductList);
     expect(wrapper.vm).toBeDefined();
@@ -18,9 +31,45 @@ describe('ProductList - integration', () => {
     const wrapper = mount(ProductList);
     expect(wrapper.findComponent(ProductCard)).toBeDefined();
   });
-  // it('should mount the ProductCard component as a child', () => {
-  //   const wrapper = mount(ProductList);
-  //   const cards = wrapper.findAllComponents(ProductCard);
-  //   expect(cards).toHaveLength(10);
-  // });
+  it('should call axios.get on component mount', () => {
+    mount(ProductList, {
+      mocks: {
+        $axios: axios,
+      },
+    });
+    expect(axios.get).toHaveBeenCalledTimes(1);
+    expect(axios.get).toHaveBeenCalledWith('/api/products');
+  });
+
+  it('should mount the ProductCard component as a child', async () => {
+    const products = server.createList('product', 10);
+
+    axios.get.mockReturnValue(Promise.resolve({ data: { products } }));
+
+    const wrapper = mount(ProductList, {
+      mocks: {
+        $axios: axios,
+      },
+    });
+
+    await wrapper.vm.$nextTick();
+
+    const cards = wrapper.findAllComponents(ProductCard);
+    expect(cards).toHaveLength(10);
+  });
+  it('should display the error message when Promise rejects', () => {
+    axios.get.mockReturnValue(
+      Promise.reject(new Error('Something went wrong'))
+    );
+
+    const wrapper = mount(ProductList, {
+      mocks: {
+        $axios: axios,
+      },
+    });
+
+    return wrapper.vm.$nextTick().then(() => {
+      expect(wrapper.text()).toContain('Problemas ao carregar os produtos');
+    });
+  });
 });
