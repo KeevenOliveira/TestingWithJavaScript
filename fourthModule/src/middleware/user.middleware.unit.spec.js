184d0b870e181f48ed7637b31d24ec70004668e1
Keeven Oliveira <keevinhuholiveira@gmail.com>
2023-02-12 17:06:00 -0300

    [UPDATE]: Add test of user middleware

---
+++ fourthModule/src/middleware/user.middleware.unit.spec.js
@@ -0,0 +1,73 @@
+import { ReasonPhrases, StatusCodes } from 'http-status-codes';
+import * as service from '@/database/service';
+import { buildError, buildNext, buildReq } from 'test/builders';
+import { get } from './user.middleware';
+
+jest.mock('@/database/service');
+
+describe('user.middleware', () => {
+  const error = buildError(
+    StatusCodes.UNPROCESSABLE_ENTITY,
+    `${ReasonPhrases.UNPROCESSABLE_ENTITY}: header should contain a valid email`,
+  );
+
+  it('should forward an error when an email is NOT provided in the headers', () => {
+    const req = buildReq({
+      headers: {},
+    });
+    const next = buildNext();
+
+    get(req, null, next);
+
+    expect(next).toHaveBeenCalledTimes(1);
+    expect(next).toHaveBeenCalledWith(error);
+  });
+
+  it('should forward an error when an email is provided in the headers but is invalid', () => {
+    const req = buildReq({
+      headers: {
+        email: 'invalid',
+      },
+    });
+    const next = buildNext();
+
+    get(req, null, next);
+
+    expect(next).toHaveBeenCalledTimes(1);
+    expect(next).toHaveBeenCalledWith(error);
+  });
+
+  it('should return an user object given a valid email is provided', async () => {
+    const req = buildReq();
+    const next = buildNext();
+    const { email } = req.headers;
+
+    const resolved = {
+      id: 1,
+      email,
+    };
+
+    jest.spyOn(service, 'findOrSave').mockResolvedValueOnce([resolved]);
+    await get(req, null, next);
+
+    expect(req.user).toEqual(resolved);
+    expect(req.user).toBeDefined();
+    expect(next).toHaveBeenCalledTimes(1);
+    expect(next).toHaveBeenCalledWith();
+  });
+
+  it('should forward an error when service.findOrSave fails', async () => {
+    const req = buildReq();
+    const next = buildNext();
+
+    delete req.user;
+
+    jest.spyOn(service, 'findOrSave').mockRejectedValueOnce('Um erro qualquer');
+
+    await get(req, null, next);
+
+    expect(req.user).toBeUndefined();
+    expect(next).toHaveBeenCalledTimes(1);
+    expect(next).toHaveBeenCalledWith('Um erro qualquer');
+  });
+});
